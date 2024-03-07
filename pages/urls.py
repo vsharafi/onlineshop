fce1f0461004d5cb41e8f792b8f74ffcff3f1a13
vahid <vahid.sh1431@yahoo.com>
2024-03-07 14:37:15 +0330

    add pages app

---
+++ pages/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import HomePageView, AboutUsView
+
+
+urlpatterns = [
+    path('', HomePageView.as_view(), name='home'),
+    path('aboutus/', AboutUsView.as_view(), name='aboutus'),
+]
